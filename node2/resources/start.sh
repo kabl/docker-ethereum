3b5818fcaf7ceea223424ddb1fa8da28259faf6a
kabl <blaser_k@gmx.ch>
2017-06-01 18:02:32 +0200

    node2: move eth-net-intelligence-api proxy

---
+++ node2/resources/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This bash script is tight bound to the Dockerfile
+set -e
+
+# startup link between netstate and geth
+cd /root/eth-net-intelligence-api
+pm2 start ./app.json
+
+# startup Geth
+/geth --datadir=/root/ethereum \
+    --verbosity 6 \
+    --identity "node2" \
+    --rpc \
+    --rpcaddr "0.0.0.0" \
+    --rpcapi "admin,eth,net,personal,web3,debug" \
+    --networkid 999 \
+    --nodiscover \
+    --maxpeers 10 \
+    --rpccorsdomain "*" \
+    js /root/addPeer.js
